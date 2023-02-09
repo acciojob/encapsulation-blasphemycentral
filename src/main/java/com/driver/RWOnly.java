534df7d6e820d92ee0dcc31d6205cfab163d8f8b
blasphemycentral <aayushnag84@gmail.com>
2023-02-09 16:54:52 +0530

    solution 1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
-public class RWOnly {
-    
+public class RWOnly
+{
+    private String name="Shreshtha";
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
